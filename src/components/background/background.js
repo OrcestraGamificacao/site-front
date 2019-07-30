0851800ae01acb3e2bd0b84b3cd2293fb98b769e
yandrade1305 <yan13052001@gmail.com>
2019-07-30 12:11:18 -0300

    Image insertion into background

---
+++ src/components/background/background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Background (){
+     const homebackground=require('../../assets/image/cave_background.svg')
+    return (
+        <div>
+            <h1>Orcestra</h1>
+        </div>
+    );
+}
+export default Background
