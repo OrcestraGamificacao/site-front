5043546804a32ac6144a6e8ee60939270b71c05c
samuel <samuel2001.furtadoavila@gmail.com>
2019-07-30 11:05:02 -0300

    criação de componente que adiciona logo na página inicial

---
+++ src/components/homeBanner/homeBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homeBanner.css'
+
+function HomeBanner() {
+    const logo=require('../../assets/images/Orc_estra_Logo1_horizontalEXP2.svg')
+  return (
+    <div className="home-banner">
+      <img className="home-logo" src={logo}/>
+    </div>
+  );
+}
+export default HomeBanner
