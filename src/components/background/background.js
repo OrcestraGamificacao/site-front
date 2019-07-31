42467c8f437cb06818f059d92b5e62817e490e8b
phe0 <pheofo@gmail.com>
2019-07-31 11:33:00 -0300

    Change style on landing section

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './background.css';
 import BackgroundImage from '../../assets/image/cave_background.svg'
 import HomeBanner from '../homeBanner/homeBanner'
+import SocialMediaButtons from '../socialMediaButtons/socialMediaButtons'
 
 function Background(){ 
     return (
         <div className="home-background">
             <img className="bg-image" src={BackgroundImage}/>
+            <div className="media-buttons" >
+                <SocialMediaButtons/>
+            </div>
             <div className="layer">
                 <HomeBanner/>
             </div>
