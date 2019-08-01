5f7e0f3405323aeb7300d128366c4a63fce8c8ac
phe0 <pheofo@gmail.com>
2019-08-01 18:02:21 -0300

    Fix sizing

--- src/components/Parceiros/Parceiros.js
+++ src/components/Parceiros/Parceiros.js
@@ -7,15 +7,17 @@ import './Parceiros.css'
 
 const Parceiros = () => (
     <div className="d-flex justify-content-center parceiros-area" >
-        <div className="parceiros d-flex justify-content-between align-items-center pt-5 pb-5 " >
-            <div className="d-flex justify-content-between flex-wrap align-items-center parceiros-imgs" >
-                <img className="parceiros-img" id="unb" src={unbImage} />
-                <img className="parceiros-img" src={fgaImage} />
-                <img className="parceiros-img" src={engageImage} />
-                <img className="parceiros-img" id="concentro" src={concentroImage} />
+        <div className="d-flex justify-content-center" >
+            <div className="parceiros d-flex justify-content-between align-items-center" >
+                <div className="d-flex justify-content-between flex-wrap align-items-center parceiros-imgs" >
+                    <img className="parceiros-img" id="unb" src={unbImage} />
+                    <img className="parceiros-img" src={fgaImage} />
+                    <img className="parceiros-img" src={engageImage} />
+                    <img className="parceiros-img" id="concentro" src={concentroImage} />
+                </div>
+                <h1 className="parceiros-text" ><b>PARCEIROS</b></h1>
             </div>
-            <h1 className="parceiros-text" ><b>PARCEIROS</b></h1>
         </div>
     </div>
-)       
+)
 export default Parceiros
