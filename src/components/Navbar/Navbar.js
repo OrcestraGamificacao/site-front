c92d15fb1a4998390ff47c63464d16d2a33c5747
Thiago <thiago099carvalho@gmail.com>
2019-08-01 12:36:23 -0300

    Added margin to navbar itens.
    Co-authored-by: Rossicler <rossiclerjr@gmail.com>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,10 +18,9 @@ class Navbar extends Component {
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
                             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
@@ -41,10 +40,6 @@ class Navbar extends Component {
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </li>
-                    </ul>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <button type="button" className="btn btn-success button-text">
                                 Diagnóstico Gratuito
