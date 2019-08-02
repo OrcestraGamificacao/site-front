2fe21df275bb6f40970c5687d895600fb8970890
Rossicler <rossiclerjr@gmail.com>
2019-08-02 17:04:09 -0300

    Fixed layout of blog cards and added link to each of them
    Co-auhoted-by: Thiago <thiago099carvalho@gmail.com>

--- src/components/blogPosts/blogPosts.js
+++ src/components/blogPosts/blogPosts.js
@@ -12,38 +12,49 @@ class BlogPosts extends Component {
 		const cat3 = require('../../assets/images/cat3.jpg')
 		return (
 			<div className="container">
-				<div className="text-center">
-					<h3>Conheça o blog</h3>
+				<div className="blog-title-container text-center">
+					<h3 className="blog-title">Conheça o blog</h3>
 				</div>
 				<div className="row">
-					<div className="col-sm-10 offset-sm-1 col-xs-10 offset-xs-1">
-						<div className="row">
-							<div className="col-sm-6 col-xs-6">
-								<div className="card">
-									<img className="card-img-top image w-100" src={cat1} alt="Card image cap" />
-									<div className="card-body p-5">
-										<h5 className="card-title">Gatos para adoção</h5>
-										<p className="card-text">Falar com Toyoshima</p>
+					<div className=" col-10 offset-1 col-sm-10 offset-sm-1 col-xs-10 offset-xs-1">
+						<div className="row h-500">
+							<div className="col-12 col-sm-5 col-xs-5">
+								<div className="row upper-card">
+									<div className="col-12 col-md-12 col-xs-12">
+										<a href="https://www.google.com.br" target="_blank">
+											<div className="card bigger-card">
+													<img className="card-img-top image w-100" src={cat1} alt="Card image cap" />
+													<div className="card-body bigger-body text-center vertical-middle">
+														<h5 className="card-title bigger-text">Postagem de Destaque</h5>
+													</div>
+											</div>
+										</a>
 									</div>
 								</div>
 							</div>
-							<div className="col-sm-6 col-xs-6">
-								<div className="row">
-									<div className="card w-100">
-										<img className="card-img-top smaller-image w-100" src={cat2} alt="Card image cap" />
-										<div className="card-body p-5">
-											<h5 className="card-title">Gatos para adoção</h5>
-											<p className="card-text">Falar com Toyoshima</p>
-										</div>
+							<div className="col-12 col-sm-7 col-xs-7">
+								<div className="row upper-card">
+									<div className="col-12 col-md-10 offset-md-1">
+										<a href="https://www.google.com.br" target="_blank">
+												<div className="card smaller-card">
+													<img className="card-img-top smaller-image w-100" src={cat2} alt="Card image cap" />
+													<div className="card-body smaller-body text-center">
+														<h5 className="card-title">Postagem de Destaque</h5>
+													</div>
+												</div>
+										</a>
 									</div>
 								</div>
 								<div className="row">
-									<div class="card">
-										<img class="card-img-top" src={cat3} alt="Card image cap" />
-										<div class="card-body">
-											<h5 className="card-title">Gatos para adoção</h5>
-											<p class="card-text">Falar com Toyoshima.</p>
-										</div>
+									<div className="col-12 col-md-10 offset-md-1">
+										<a href="https://www.google.com.br" target="_blank">
+											<div class="card smaller-card">
+												<img class="card-img-top smaller-image w-100" src={cat3} alt="Card image cap" />
+												<div class="card-body smaller-body text-center">
+													<h5 className="card-title">Postagem de Destaque</h5>											
+												</div>
+											</div>
+										</a>
 									</div>
 								</div>
 							</div>
