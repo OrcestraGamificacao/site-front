0221ccb54e897c426ac3b07175787adf171ad7ad
Rossicler <rossiclerjr@gmail.com>
2019-08-02 15:31:19 -0300

    Changed some grid layout
    Co-authored-by: Thiago <thiago099carvalho@gmail.com>

--- src/components/blogPosts/blogPosts.js
+++ src/components/blogPosts/blogPosts.js
@@ -11,35 +11,39 @@ class BlogPosts extends Component {
 		const cat2 = require('../../assets/images/cat2.jpg')
 		const cat3 = require('../../assets/images/cat3.jpg')
 		return (
-			<div>
+			<div className="container">
 				<div className="text-center">
 					<h3>Conheça o blog</h3>
 				</div>
-				<div className="row mx-auto">
-					<div className="col-sm-10 offset-sm-2 col-xs-10 offset-xs-2">
-						<div className="card col-sm-6 col-xs-6 card-center-left">
-							<img className="card-img-top image w-100" src={cat1} alt="Card image cap" />
-							<div className="card-body p-5">
-								<h5 className="card-title">Gatos para adoção</h5>
-								<p className="card-text">Falar com Toyoshima</p>
-							</div>
-						</div>
-						<div className="col-sm-6 col-xs-6">
-							<div className="row">
-								<div className="card w-100 card-center-right">
-									<img className="card-img-top smaller-image w-100" src={cat2} alt="Card image cap" />
+				<div className="row">
+					<div className="col-sm-10 offset-sm-1 col-xs-10 offset-xs-1">
+						<div className="row">
+							<div className="col-sm-6 col-xs-6">
+								<div className="card">
+									<img className="card-img-top image w-100" src={cat1} alt="Card image cap" />
 									<div className="card-body p-5">
 										<h5 className="card-title">Gatos para adoção</h5>
 										<p className="card-text">Falar com Toyoshima</p>
 									</div>
 								</div>
 							</div>
-							<div className="row">
-								<div class="card card-center-right">
-									<img class="card-img-top" src={cat3} alt="Card image cap" />
-									<div class="card-body">
-										<h5 className="card-title">Gatos para adoção</h5>
-										<p class="card-text">Falar com Toyoshima.</p>
+							<div className="col-sm-6 col-xs-6">
+								<div className="row">
+									<div className="card w-100">
+										<img className="card-img-top smaller-image w-100" src={cat2} alt="Card image cap" />
+										<div className="card-body p-5">
+											<h5 className="card-title">Gatos para adoção</h5>
+											<p className="card-text">Falar com Toyoshima</p>
+										</div>
+									</div>
+								</div>
+								<div className="row">
+									<div class="card">
+										<img class="card-img-top" src={cat3} alt="Card image cap" />
+										<div class="card-body">
+											<h5 className="card-title">Gatos para adoção</h5>
+											<p class="card-text">Falar com Toyoshima.</p>
+										</div>
 									</div>
 								</div>
 							</div>
