6a67f7e1a8a6fb030df02631d557d66f33aa547d
Thiago <thiago099carvalho@gmail.com>
2019-08-02 18:04:10 -0300

    Creat new page contact

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+
+    render(){
+        return(
+            <div>
+                lalala
+            </div>
+        );
+    }
+}
+export default Contact;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import App, {About} from './components/App'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from './pages/home/home'
+import Contact from './pages/contact';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/contato' component={Contact} />
         </BrowserRouter>
     );
 }
