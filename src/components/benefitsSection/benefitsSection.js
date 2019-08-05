55ff1d8e7da65903e342d381953b90ef11ba31d8
Guilherme <marquesguilherme3@gmail.com>
2019-08-05 00:41:42 -0300

    section end

--- src/components/benefitsSection/benefitsSection.js
+++ src/components/benefitsSection/benefitsSection.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react'
 import './benefitsSection.css'
 import BenefitCard from '../benefitCard/benefitCard';
+import BtnServices from '../btnServices/BtnServices';
 
 const benefits = [
   {
-    title: "Something",
-    body: "More explanation here!",
-    icon: require("../../assets/images/pato_orc.png")
+    title: "Engajamento",
+    body:
+      "Pooping rainbow while flying in a toasted bread costume in space chase ball of string chew foot, and poop on grasses. Why must they do that flop over. Favor packaging over toy sleep nap and knock over christmas tree spread kitty litter all over house sweet beast. Use lap as chair give attitude poop on grasses sleep nap. Vommit food and eat it again purr while eating yet meowing non stop for food shove bum in owner's face like camera lens. ",
+    icon: require("../../assets/images/icon_mock.png")
   },
   {
-    title: "Something",
-    body: "More explanation here!",
-    icon: require("../../assets/images/pato_orc.png")
+    title: "Motivação",
+    body: "Pooping rainbow while flying in a toasted bread costume in space chase ball of string chew foot, and poop on grasses. Why must they do that flop over. Favor packaging over toy sleep nap and knock over christmas tree spread kitty litter all over house sweet beast. Use lap as chair give attitude poop on grasses sleep nap. Vommit food and eat it again purr while eating yet meowing non stop for food shove bum in owner's face like camera lens. ",
+    icon: require("../../assets/images/icon_mock.png")
   },
   {
-    title: "Something",
-    body: "More explanation here!",
-    icon: require("../../assets/images/pato_orc.png")
+    title: "Produtividade",
+    body: "Pooping rainbow while flying in a toasted bread costume in space chase ball of string chew foot, and poop on grasses. Why must they do that flop over. Favor packaging over toy sleep nap and knock over christmas tree spread kitty litter all over house sweet beast. Use lap as chair give attitude poop on grasses sleep nap. Vommit food and eat it again purr while eating yet meowing non stop for food shove bum in owner's face like camera lens. ",
+    icon: require("../../assets/images/icon_mock.png")
   }
 ];
 
@@ -46,7 +48,11 @@ class BenefitsSection extends Component {
                 <div className='benefits-cards'>
                     {this.renderCards()}
                 </div>
+
+                <BtnServices />
+
             </section>
+            <div className='benefits-end' />
             </>
         )
     }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -49,8 +49,7 @@ class Home extends Component {
 
                 <BenefitsSection />
 
-                <BtnServices/>                
-                <SuccessCase/>                
+                <SuccessCase/>
                 <AboutUs/>
                 <Parceiros/>
                 <Statistics/>
