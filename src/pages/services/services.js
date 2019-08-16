6f04451a017607fd7379848abd9ef9469d8f1bb8
Alan Marques Sousa <alan.sousa@protonmail.com>
2019-08-16 10:03:11 -0300

    Criacao da tela de servicos

---
+++ src/pages/services/services.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react'
+import NavBar from '../../components/Navbar/Navbar'
+import group from '../../assets/images/Group.svg'
+import group2 from '../../assets/images/Group 2.svg'
+import group2_1 from '../../assets/images/Group 2-1.svg'
+import group3 from '../../assets/images/Group 3.svg'
+import './services.css'
+
+class Services extends Component {
+
+   render() {
+      return (
+         <>
+            <NavBar/>
+            <div className="container-fluid background-services">
+               <h1 className="title-service text-center">Entenda um pouco sobre nossos serviços!</h1>
+               <div className="service row justify-content-md-center">
+                  <img className="img-service float-left" src={group} alt="group"/>
+                  <div className="desc-service float-left">
+                     <h2 className="title-desc-service text-center">
+                        Design da Gamificação
+                     </h2>
+                     <p className="text-desc-service text-center">
+                        Identificamos e analisamos problemas no contexto da sua empresa para planejar e elaborar uma gamificação personalizada.
+                     </p>
+                  </div>
+               </div>
+               <div className="service row justify-content-md-center">
+                  <div className="desc-service float-left">
+                     <h3 className="title-desc-service text-center">
+                        Desenvolvimento da Gamificação
+                     </h3>
+                     <p className="text-desc-service text-center">
+                        Desenvolvemos um software a partir de uma gamificação personalizada para o seu contexto, ou realizamos atualização e manutenção de softwares já existentes, afim de melhorar uma gamificação.
+                     </p>
+                  </div>
+                  <img className="img-service float-left" src={group2} alt="group"/>
+               </div>
+               <div className="service row justify-content-md-center">
+                  <img className="img-service float-left" src={group2_1} alt="group"/>
+                  <div className="desc-service float-left">
+                     <h2 className="title-desc-service text-center">
+                        Treinamento
+                     </h2>
+                     <p className="text-desc-service text-center">
+                        Realizamos treinamentos gamificados acerca de gamificação e conteúdos na área de Engenharia de Software.
+                     </p>
+                  </div>
+               </div>
+               <div className="service row justify-content-md-center">
+                  <div className="desc-service float-left">
+                     <h3 className="title-desc-service text-center">
+                        Consultoria
+                     </h3>
+                     <p className="text-desc-service text-center">
+                        Identificamos problemas e sugerimos melhorias em gamificações já existentes ou ainda em andamento.
+                     </p>
+                  </div>
+                  <img className="img-service float-left" src={group3} alt="group"/>
+               </div>
+            </div>
+         </>
+      )
+   }
+}
+
+export default Services
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import App, { About, Services, Portfolio, Blog } from './components/App'
+import App, { About, Portfolio, Blog } from './components/App'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from './pages/home/home'
+import Services from './pages/services/services'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
-            <Route exact path='/services' component={About} />
+            <Route exact path='/services' component={Services} />
             <Route exact path='/portfolio' component={About} />
             <Route exact path='/blog' component={About} />
         </BrowserRouter>
