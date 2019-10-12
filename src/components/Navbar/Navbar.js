507eb2b1633f6398ab898a513dcbbe2e7b73cc61
phe0 <pheofo@gmail.com>
2019-10-12 18:07:55 -0300

    fix navbar collapse

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,11 +11,11 @@ class Navbar extends Component {
                     <img className="logo" src={logo}/>
                 </Link>
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse">
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <Link to="/services">
